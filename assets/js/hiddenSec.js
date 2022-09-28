a01fec75449d64fbd9412ec70dbcb06d6dae5dda
fannypag <81959515+fannypag@users.noreply.github.com>
2022-09-28 12:26:54 +0700

    .

--- assets/js/hiddenSec.js
+++ assets/js/hiddenSec.js
@@ -1,4 +1,6 @@
+
 $(document).ready(function(){
+    //SECTION ISI ARTIKEL
     $('#flora').hide();
     $('#boxFlora').click(function(){
         $('#flora').show();
@@ -7,6 +9,24 @@ $(document).ready(function(){
         }, 2000);
     })
 
+    $('#fauna').hide();
+    $('#boxFauna').click(function(){
+        $('#fauna').show();
+        $('html, body').animate({
+            scrollTop: $("#fauna").offset().top
+        }, 2000);
+    })
+
+    $('#wisata').hide();
+    $('#boxWisata').click(function(){
+        $('#wisata').show();
+        $('html, body').animate({
+            scrollTop: $("#wisata").offset().top
+        }, 2000);
+    })
+
+
+    // SECTION ISI FLORA
     $('#bungabangkai').hide();
     $('#boxBungabangkai').click(function(){
         $('#bungabangkai').show();
@@ -14,5 +34,12 @@ $(document).ready(function(){
             scrollTop: $("#flora").offset().top
         }, 2000);
     })
+
+    // SECTION ISI FAUNA
+    
+
+
+
+    // SECTION ISI WISATA
 
 })
