498b22a9ebffe9e8f42ae3d8b4c6f89d5673b915
rifatiyay <salmarifatia@gmail.com>
2022-10-03 17:46:13 +0700

    .

--- assets/js/hiddenSec.js
+++ assets/js/hiddenSec.js
@@ -42,8 +42,70 @@ $(document).ready(function(){
     })
 
     // SECTION ISI FAUNA
-    
+    $('#bantengjawa').hide();
+    $('#BoxBantengJawa').click(function(){
+        $('#bantengjawa').show();
+        // $("html, body").animate({
+        //     scrollTop: $("#flora").offset().top
+        // }, 2000);
 
+        $('#burungjulang').hide();
+        $('#babun').hide();
+        $('#katak').hide();
+        $('#ikantongkol').hide();
+    })
+
+    $('#burungjulang').hide();
+    $('#BoxJulangEmas').click(function(){
+        $('#burungjulang').show();
+        // $("html, body").animate({
+        //     scrollTop: $("#flora").offset().top
+        // }, 2000);
+
+        $('#bantengjawa').hide();
+        $('#babun').hide();
+        $('#katak').hide();
+        $('#ikantongkol').hide();
+    })
+
+    $('#babun').hide();
+    $('#BoxBabun').click(function(){
+        $('#babun').show();
+        // $("html, body").animate({
+        //     scrollTop: $("#flora").offset().top
+        // }, 2000);
+
+        $('#bantengjawa').hide();
+        $('#burungjulang').hide();
+        $('#katak').hide();
+        $('#ikantongkol').hide();
+    })
+
+    $('#katak').hide();
+    $('#BoxKatak').click(function(){
+        $('#katak').show();
+        // $("html, body").animate({
+        //     scrollTop: $("#flora").offset().top
+        // }, 2000);
+
+        $('#bantengjawa').hide();
+        $('#burungjulang').hide();
+        $('#babun').hide();
+        $('#ikantongkol').hide();
+    })
+
+    $('#ikantongkol').hide();
+    $('#BoxTongkol').click(function(){
+        $('#ikantongkol').show();
+        // $("html, body").animate({
+        //     scrollTop: $("#flora").offset().top
+        // }, 2000);
+
+        $('#bantengjawa').hide();
+        $('#burungjulang').hide();
+        $('#babun').hide();
+        $('#katak').hide();
+    })
 
 
     // SECTION ISI WISATA
